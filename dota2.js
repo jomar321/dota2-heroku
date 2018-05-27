3e558b05af21e3ec710d75e6fff906ff7a972f16
jomar321 <39395460+jomar321@users.noreply.github.com>
2018-05-27 21:33:51 +0800

    Create dota2.js

---
+++ dota2.js
@@ -0,0 +1,14 @@
+const Discord = require('Discord.js')
+const client = new Discord.Client();
+
+if(command === "@say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+  
+  client.login(process.env.BOT_TOKEN);
