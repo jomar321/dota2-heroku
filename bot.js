a4f0a7266d8f37f8a78595cfecd4343b4c686bba
jomar321 <39395460+jomar321@users.noreply.github.com>
2018-05-28 00:18:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 const Discord = require('Discord.js')
 const client = new Discord.Client();
 
-if(command === "@say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = (" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
-  
-  client.login(process.env.BOT_TOKEN);
+client.on('message', msg => {
+    if(msg.content === "@say") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+        const sayMessage = (" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        msg.delete().catch(O_o=>{}); 
+        // And we get the bot to say the thing: 
+        msg.channel.send(sayMessage);
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
